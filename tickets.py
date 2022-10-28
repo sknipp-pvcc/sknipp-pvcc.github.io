1dd4acdc43968c8226e09ccea7e6f6457e6fbbf3
sknipp-pvcc <114419341+sknipp-pvcc@users.noreply.github.com>
2022-10-27 21:23:28 -0400

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -37,9 +37,9 @@ def display_results():
     print('*****CINEMA HOUSE MOVIES*****')
     print('Your neighborhood movie house')
     print('-------------------------------')
-    print('Tickets      $ ' + format(subtotal, '8,.2f'))
-    print('Sales Tax    $ ' + str(sales_tax,'8,.2f'))
-    print('Total        $ ' + str(total,'8,.2f'))
+    print('Tickets      $ ' + format(subtotal,'8,.2f'))
+    print('Sales Tax    $ ' + format(sales_tax,'8,.2f'))
+    print('Total        $ ' + format(total,'8,.2f'))
     print('-------------------------------')
     print(str(datetime.datetime.now()))
 
