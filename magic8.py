42fd3696281063a1eb68b3971d13f8e1e2bba641
sknipp-pvcc <114419341+sknipp-pvcc@users.noreply.github.com>
2022-11-09 14:14:26 -0500

    Create magic8.py

---
+++ magic8.py
@@ -0,0 +1,50 @@
+# Name: Sarah Knipp
+#Prog Purpose: This Magic 8-Ball code uses a Python tuple since the user does not have the ability to change the 8ball answers.
+# However the program could have used a python list instead of a tuple.
+#Note: Tuples use parenthese; list use square braces.
+
+import random
+answers_8_ball = (
+    "As I see it, yes", 
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "It is certain",
+    "It is decidedly so",
+    "Most likely",
+    "My reply is no",
+    "My sources say no",
+    "Outlook good",
+    "Outlook not so good",
+    "Reply hazy try again",
+    "Signs point to yes",
+    "Very doubtful",
+    "Without a doubt",
+    "Yes",
+    "Yes, definately",
+    "You may rely on it",
+    )
+
+def main():
+
+    print("I am the MAGIC 8-BALL and can answer any YES or NO question!")
+
+    another_question = True
+    while another_question:
+        answer = random.choice(answers_8_ball)
+
+        print("\nShake the MAGIC 8-BALL")
+        print("...and now...")
+        question = input ("What is your YES or NO question? ")
+        print("The MAGIC-8 ball says: " + answer)
+
+        askAgain = input("\nWould you like to ask me another question (Y or N) ?: ")
+        if askAgain.upper () == "N" or askAgain == "n":
+            another_question = False
+
+    print("\nCome back again when you have other important questions.")
+    print("Magic-* Ball OUT.")
+
+main()
