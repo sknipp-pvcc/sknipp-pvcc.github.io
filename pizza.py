c9d7ece3bc622045b99b08bd472dc9f95f8e732d
sknipp-pvcc <114419341+sknipp-pvcc@users.noreply.github.com>
2022-11-17 19:10:34 -0500

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,69 +1,77 @@
-import time
 import datetime
 
-#global list (variable)
-order = [0,0,0,0]
-sales_tax = .055
-total = 0
-#order column 0 is small col 1 is med col 2 is large col 3 is xl
+hourly_rate = (16.50, 15.75, 15.75, 19.58)
+deductions = (.12, .03, .062, .0145)
+fed_tax = 0
+state_tax = 0
+ss_tax = 0
+medicare_tax = 0
+total_deductions = 0
+net_pay = 0
+job_cat = ""
+hours = 0
+pay_rate = 0
 
-#definitions of functions
-def main():
-    global order, sales_tax, total
-    while True:
-        add_pizza()
-        option = str(input("Do you want another pizza?(yes/no)"))
-        if option == "n" or option == "no" or option == "No":
-            break
-    recipit()
+def get_employee_data():
+    global hours, job_cat
+    hours = int(input("How many hours has employee worked?:   "))
+    job_cat = str(input("What is your job category? C: Cashier, J: Janitor, S: Stoker, M: Mainenance"))
+    
+def perform_calculations():
+    global pay_rate, gross_pay, net_pay, total_deductions, fed_tax, state_tax, ss_tax, medicare_tax
+    if job_cat == "Cashier" or job_cat.upper() == "C":
+        gross_pay = hourly_rate[0] * hours
+        pay_rate = hourly_rate[0]
+    elif job_cat == "Stocker":
+        gross_pay = hourly_rate[1] * hours
+        pay_rate = hourly_rate[1]
 
+    elif job_cat == "Janitor":
+        gross_pay = hourly_rate[2] * hours
+        pay_rate = hourly_rate[2]
+    elif job_cat == "Maintenance":
+        gross_pay = hourly_rate[3] * hours
+        pay_rate = hourly_rate [3]
+    
+    total_deductions = (gross_pay * deductions[0]) + (gross_pay * deductions[1]) + (gross_pay * deductions[2]) + (gross_pay * deductions[3])
+    fed_tax = gross_pay * deductions[0]
+    state_tax = gross_pay * deductions[1]
+    ss_tax = gross_pay * deductions[2]
+    medicare_tax = gross_pay* deductions[3]
+    net_pay = gross_pay - total_deductions
+    
+    
+   
 
-
-def add_pizza():
-    global order
-    za = str(input("What size pizza would you like? Small, Med, Lg, XL?"))
-    if za == "small" or za == "s" or za == "Small" or za == "sm" or za == "SMall":
-        count = int(input("How many you want?:   "))
-        order[0] = order[0] + count 
-    #col 0 is small
-    elif za == "Med" or za == "med" or za == "M" or za == "m" or za == "Medium" or za == "medium":
-        count = int(input("How many of this size would you like?:   "))
-        order[1] = order[1] + count  
-    #col 1 is med
-    elif za == "large" or za == "lg" or za == "Large" or za == "LG" or za == "LARGE" or za == "lARGE":
-        count = int(input("How many of this size would you like?:   "))
-        order[2] = order[2] + count  
-    #col 2 is large
-    elif za == "x" or za == "X" or za == "XL" or za == "xl" or za == "Extra Large" or za == "extra large":
-        count = int(input("How many of this size would you like?:   "))
-        order[3] = order[3] + count  
-    #col 3 is xl
-    else: 
-        print("Incorrect input. This selection was not added to your order... You are being returned to pizza menu prior to this interaction.")
-        time.sleep(3)
-
-def recipit():
-    global order, sales_tax, total
-    smallTotal = 9.99 * order[0]
-    mediumTotal = 12.99 * order[1]
-    largeTotal = 14.99 * order[2]
-    xlargeTotal = 17.99 * order[3]
-    sales_tax = (smallTotal + mediumTotal + largeTotal + xlargeTotal) * .055
-    total = smallTotal + mediumTotal + largeTotal + xlargeTotal + sales_tax
+def paystub():
     print('-------------------------------')
-    print('********PALERMO PIZZA********')
-    print('Your neighborhood pizza joint')
+    print('Fresh Foods Employee Weekly Pay')
+    print('*******Employee Pay Stub*******')
     print('-------------------------------')
-    print('Small        $ ' + format(smallTotal,'8,.2f'))
-    print('Med          $ ' + format(mediumTotal,'8,.2f'))
-    print('Large        $ ' + format(largeTotal,'8,.2f'))
-    print('XLarge       $ ' + format(xlargeTotal,'8,.2f'))
-    print('Sales Tax    $ ' + format(sales_tax,'8,.2f'))
-    print('Total        $ ' + format(total,'8,.2f'))
+    print('Job Title        '+ job_cat)
+    print('Pay Rate         $'+ format(pay_rate,'8,.2f'))
+    print('Hours             '+ format(hours,'8,.2f'))
+    print('Federal Tax      $'+ format(fed_tax,'8,.2f'))
+    print('State Tax        $'+ format(state_tax,'8,.2f'))
+    print('Social Security  $'+ format(ss_tax,'8,.2f'))
+    print('Medicare         $'+ format(medicare_tax,'8,.2f'))
+    print('Gross Pay        $'+ format(gross_pay,'8,.2f'))
+    print('Total Deductions $'+ format(total_deductions,'8,.2f'))
+    print('Net Pay          $'+ format(net_pay,'8,.2f'))
     print('-------------------------------')
     print(str(datetime.datetime.now()))
     print("Thank you for your business!")
 
 
-main()
+
+def main():
+    while True:
+        get_employee_data()
+        perform_calculations()
+        paystub()
+        option = str(input("Would you like an additional paystub for another employee? "))
+        if option == "n" or option == "no" or option == "No":
+            break
+
     
+main()
