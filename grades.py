9e3cb4dd311d538c33291e55e874c061df18247d
sknipp-pvcc <114419341+sknipp-pvcc@users.noreply.github.com>
2022-11-30 10:12:54 -0500

    Update grades.py

--- grades.py
+++ grades.py
@@ -1,75 +1,85 @@
-#Name: Sarah Knipp
-#Prog Purpose: This program computes grades for student using a 
-#   Python list of data in Python Dictionaries (as student records)
-#   Each student has 3 graded items: Howework, Midterm exam, and Fial exam
-#Each item is worth 100 points.
-#The students final grade is the average of the three graded items.
+# Name : Sarah Knipp
+# Prog Purpose : This program reads a comma- delimited data file into a LIST of smaller lists .
+#   The data for each student is appended to a list with six data fields:
+#   Postion 0 is last name l is fist name and 2 is homework, 4 is midterm exam 4 is final exam 
+#   The group of student lists in the larger "outer" list
+#   The resulting report is sent to an output file. 
 
-stu = [
-    {'name' : "Anderson, Manuel",
-    'hw' : 89,
-    'mid_exam' : 92,
-    'fin_exam' : 94,
-    'fin_grade' : 0,
-    'ltr_grade': "X",
-    },
-   
-   {'name' : "Thompson, Yani",
-    'hw' : 63,
-    'mid_exam' : 74,
-    'fin_exam' : 59,
-    'fin_grade' : 0,
-    'ltr_grade': "X",
-    },
-
-    {'name' : "Ortiz, Graciela",
-    'hw' : 81,
-    'mid_exam' : 76,
-    'fin_exam' : 79,
-    'fin_grade' : 0,
-    'ltr_grade': "X",
-    },
-
-    {'name' : "Wilson, Danielle",
-    'hw' : 100,
-    'mid_exam' : 89,
-    'fin_exam' : 94,
-    'fin_grade' : 0,
-    'ltr_grade': "X",
-    },
-]
+import datetime
+stu = []
+student_file = "students.csv"
+out_file = "stu_report.txt"
 
 def main():
+    read_data_file()
     calculate_grades()
-    print_grade_report()
+    create_report_file()
+
+def read_data_file():
+    datafile = open(student_file,"r") #the "r" indicates this is a READ file
+    lines = datafile.readlines() #Read the entire file into a variable called lines
+    for i in lines:
+        stu.append(i.split(",")) #Split the data by commas and add each section to the list
+    datafile.close()
 
 def calculate_grades():
-    for i in range(len(stu)):
-        sum_grades = stu[i]['hw'] + stu[i]['mid_exam'] + stu[i]['fin_exam']
-        stu[i]['fin_grade'] = sum_grades / 3
+    global average, totA, totB, totC, totD, totF
+    totA = totB = totC = totD = totF = 0
+    gradestotal = 0
 
-        finalgr = stu[i]['fin_grade']
+    for i in range(len(stu)):
+        hw = int(stu[i][2]) #Last name is pos. O, first name is pos. 1, homework is pos. 2
+        midex= int(stu[i][3]) #Midterm exam is in position 3
+        stu[i][4] = stu[i][4][:-1] #Remove the \n from the final exam string (end of the input line)
+        finex = int(stu[i][4]) #final exam is in position 4
+        sum_grades = hw + midex + finex
+        finalgr = sum_grades / 3
+        gradestotal += finalgr # Add the grade to the total for calculating the average
+        stu[i].append(finalgr)
+
         if finalgr >= 90:
             lettergrade = 'A'
+            totA += 1
         elif finalgr >= 80:
             lettergrade = 'B'
+            totB += 1
         elif finalgr >= 70:
             lettergrade = 'C'
+            totC += 1
         elif finalgr >= 60:
             lettergrade = 'D'
-        else: 
-            lettergrade = 'B'
-        stu[i]['ltr_grade'] = lettergrade
+            totD += 1
+        else:
+            lettergrade = 'F'
+            totF += 1
+        stu[i].append(lettergrade)
 
-def print_grade_report():
-    print("*******************CSC 230 Grade Report*******************")
-    print("Name                     HW\tMid\tFin\tFinal Grade")
+    average = gradestotal / len(stu)
+
+def create_report_file():
+    line = "\n************************************************************"
+    repfile = open(out_file, "w")
+    repfile.write("\n*******************CSC 230 Grade Report*******************")
+    repfile.write("\nReport Date/Time          :" + str(datetime.datetime.now()))
+    repfile.write("\nTotal number of students  :" + str(len(stu)))
+    repfile.write("\nAverage Student Grade     :" + format(average,'5,.2f'))
+    repfile.write("\nTotal number of A grades  :" + str(totA))
+    repfile.write("\nTotal number of B grades  :" + str(totB))
+    repfile.write("\nTotal number of C grades  :" + str(totC))
+    repfile.write("\nTotal number of D grades  :" + str(totD))
+    repfile.write("\nTotal number of F grades  :" + str(totF))
+    repfile.write(line)
+    repfile.write("\nName                             HW\tMID\tFIN\tFINAL GRADE")
+    repfile.write(line)
     for i in range(len(stu)):
-        sname = '{0: <18}'.format(stu[i]['name'])
-        hw = str(stu[i]['hw'])
-        midex = str(stu[i]['mid_exam'])
-        finex = str(stu[i]['fin_exam'])
-        fingrade = format(stu[i]['fin_grade'], '6,.2f')
-        print ( sname + "\t" + hw + "\t" + midex + "\t" + finex + "\t" + fingrade + "\t" + " " + stu[i]['ltr_grade'] )
+        sname = '{0:<18}'.format(stu[i][0]+', '+ stu [i][1])
+        hw = str(stu[i][2])
+        midex = str(stu[i][3])
+        finex = str(stu[i][4])
+        fingrade = format(stu[i][5],'6,.2f')
+        repfile.write("\n" + sname + "\t" + hw + "\t" + midex + "\t" + finex + "\t" + fingrade + "\t" + stu[i][6])
+    repfile.write(line)
+    repfile.close()
+    print("open this file to view the Grades Report: " + out_file)
 
 main()
